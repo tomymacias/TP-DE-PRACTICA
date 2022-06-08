29f2ca11006580f5b2d97d2ec6de0d7ed8e50917
Tomas Macias <tmaciastba.edu.ar>
2022-06-08 18:56:09 -0300

    use prettier para las buenas practicas

--- menu.js
+++ menu.js
@@ -1,33 +1,32 @@
 var btnmenu = document.getElementById(`btn-menu`);
 var nav = document.getElementById(`nav`);
-btnmenu.addEventListener(`click`, function(){
-    nav.classList.toggle(`muestrate`)
-})
+btnmenu.addEventListener(`click`, function () {
+  nav.classList.toggle(`muestrate`);
+});
 
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
 
-const newYear = '11 Jul 2022';
+const newYear = "11 Jul 2022";
 
-function countTimer(){
-    const newYearDate = new Date(newYear);
-    const currentDate = new Date();
+function countTimer() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
 
-    const totalSeconds = (newYearDate - currentDate) / 1000;
+  const totalSeconds = (newYearDate - currentDate) / 1000;
 
-    const daysCalc = Math.floor(totalSeconds / 3600 / 24);
-    const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
-    const minsCalc = Math.floor(totalSeconds / 60) % 60;
-    const secondsCalc = Math.floor(totalSeconds % 60);
-    
-    days.innerHTML = daysCalc;
-    hours.innerHTML = hoursCalc;
-    mins.innerHTML = minsCalc;
-    seconds.innerHTML = secondsCalc;
+  const daysCalc = Math.floor(totalSeconds / 3600 / 24);
+  const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
+  const minsCalc = Math.floor(totalSeconds / 60) % 60;
+  const secondsCalc = Math.floor(totalSeconds % 60);
+
+  days.innerHTML = daysCalc;
+  hours.innerHTML = hoursCalc;
+  mins.innerHTML = minsCalc;
+  seconds.innerHTML = secondsCalc;
 }
 countTimer();
 
 setInterval(countTimer, 1000);
-
